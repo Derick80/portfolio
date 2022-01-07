4cd165ed2c5bbe3234056eaad91aea2720e0fa59
DCH1980 <derickchoskinson@gmail.com>
2022-01-06 22:55:09 -0600

    thursday commits

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_links');
-    const navLinks = document.querySelectorAll('.nav_links li')
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav_links')
+  const navLinks = document.querySelectorAll('.nav_links li')
 
-
-burger.addEventListener('click', () => {
-
-        nav.classList.toggle('nav_active')
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
-            }
-        })
-
-        burger.classList.toggle('toggle');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav_active')
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`
+      }
     })
 
+    burger.classList.toggle('toggle')
+  })
 }
 
 navSlide()
 
-window.addEventListener('scroll', function() {
-    var nav = document.querySelector("nav")
-    nav.classList.toggle('sticky',window.scrollY > 0);
+window.addEventListener('scroll', function () {
+  var nav = document.querySelector('nav')
+  nav.classList.toggle('sticky', window.scrollY > 0)
 })
 
-
-const switchElement = document.querySelector('.switch')
+const switchElement = document.querySelector('.flicker')
 
 switchElement.addEventListener('click', () => {
-    document.body.classList.toggle('dark')
+  document.body.classList.toggle('dark')
 })
 
+window.addEventListener('scroll', function () {
+  var backtoTop = document.querySelector('.switch')
+  backtoTop.classList.toggle('glue', window.scrollY > 200)
+})
